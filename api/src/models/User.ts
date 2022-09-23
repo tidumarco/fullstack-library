22f26243dc872f93815788b3b1e63c309a0ffb7d
Marco Tidu <matidu@utu.fi>
2022-09-23 15:03:28 +0300

    Added isAdmin boolean to user model

--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -1,6 +1,7 @@
 import mongoose, { Document, Date } from 'mongoose'
 
 export type UserDocument = Document & {
+  isAdmin: boolean
   userId: string
   firstName: string
   lastName: string
@@ -9,6 +10,7 @@ export type UserDocument = Document & {
 }
 
 const userSchema = new mongoose.Schema({
+  isAdmin: Boolean,
   userId: String,
   firstName: String,
   lastName: String,
