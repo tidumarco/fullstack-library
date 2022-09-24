95642a427f0264af8e288afacc0da831f62b3281
Marco Tidu <matidu@utu.fi>
2022-09-24 10:05:06 +0300

    Styled table header

--- client/src/pages/BooksTable.tsx
+++ client/src/pages/BooksTable.tsx
@@ -15,34 +15,46 @@ import {
 } from "@mui/material";
 
 export default function BooksTable({ filter }: BasicTable) {
-	const dispatch = useDispatch<AppDispatch>();
+  const dispatch = useDispatch<AppDispatch>();
   const { books } = useSelector((state: RootState) => state);
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>ISBN</TableCell>
-            <TableCell>Title</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>Authors</TableCell>
-            <TableCell>Borrower ID</TableCell>
-            <TableCell>Admin ID</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {filter.map((book: Book) => (
-            <TableRow key={book.ISBN}>
-              <TableCell align="right">{book.ISBN}</TableCell>
-              <TableCell align="right">{book.title}</TableCell>
-              <TableCell align="right">{book.description}</TableCell>
-              <TableCell align="right">{book.authors}</TableCell>
-              <TableCell align="right">{book.borrowerId}</TableCell>
-              <TableCell align="right">{book.adminId}</TableCell>
+    <>
+      <Typography>{books.isLoading && "Loading books"}</Typography>
+      <TableContainer component={Paper}>
+        <Table sx={{ tableLayout: "auto" }} aria-label="simple table">
+          <TableHead>
+            <TableRow
+              sx={{
+                
+                borderBottom: "2px solid black",
+                "& th": {
+                  fontSize: "1.25rem",
+                  color: "rgba(96, 96, 96)",
+                },
+              }}
+            >
+              <TableCell>ISBN</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Authors</TableCell>
+              <TableCell>Borrower ID</TableCell>
+              <TableCell>Admin ID</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {filter.map((book: Book) => (
+              <TableRow key={book.ISBN}>
+                <TableCell align="right">{book.ISBN}</TableCell>
+                <TableCell align="right">{book.title}</TableCell>
+                <TableCell align="right">{book.description}</TableCell>
+                <TableCell align="right">{book.authors}</TableCell>
+                <TableCell align="right">{book.borrowerId}</TableCell>
+                <TableCell align="right">{book.adminId}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
